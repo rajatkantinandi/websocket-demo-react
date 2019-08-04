fd5bbc619fcab585a44f7f11356391b20a496f80
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2019-08-04 18:54:33 +0530

    Websocket test init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [socket] = useState(new WebSocket('ws://dot-show.glitch.me/echo'));
+  const [message, setMessage] = useState('');
+  const [responses, setResponses] = useState([]);
+
+  socket.addEventListener('open', function (event) {
+    console.log('Connection Established!');
+  });
+
+  socket.addEventListener('message', function (event) {
+    setResponses([...responses, { message: event.data, from: 'server' }]);
+  });
+
+  const sendMessage = (ev) => {
+    ev.preventDefault();
+    setResponses([...responses, { message: 'You-> ' + message, from: 'you' }]);
+    socket.send(message);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Websocket Echo Test</h1>
       </header>
+      <main>
+        <form onSubmit={sendMessage}>
+          <label htmlFor="message">Send Message to Server</label><br /><br />
+          <input
+            type="text"
+            id="message"
+            placeholder="Enter message to send"
+            value={message}
+            onChange={(ev) => setMessage(ev.target.value)}
+            autoFocus
+            autoComplete="off" /> &nbsp;
+          <button>Send</button>
+        </form>
+        <hr />
+        {responses.length > 0 &&
+          <ul>
+            <h2>Responses</h2>
+            {
+              responses.map((response, idx) => (
+                <li key={idx} className={'message ' + response.from}>{response.message}</li>
+              ))
+            }
+          </ul>
+        }
+      </main>
     </div>
   );
 }
