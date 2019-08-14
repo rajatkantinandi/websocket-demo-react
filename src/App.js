a45a16b365c409ea8addd0f00a754a09779470c4
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2019-08-14 18:29:09 +0530

    UI tweaks + scrollToBottom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, createRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [socket] = useState(new WebSocket('ws://dot-show.glitch.me/echo'));
   const [message, setMessage] = useState('');
   const [responses, setResponses] = useState([]);
+  const [isSocketConnected, setSocketConnected] = useState(false);
+  const responsesRef = createRef();
 
   socket.addEventListener('open', function (event) {
-    console.log('Connection Established!');
+    setSocketConnected(true);
   });
 
   socket.addEventListener('message', function (event) {
@@ -18,7 +20,14 @@ function App() {
     ev.preventDefault();
     setResponses([...responses, { message: 'You-> ' + message, from: 'you' }]);
     socket.send(message);
+    setMessage('');
   };
+
+  useEffect(() => {
+    if (responsesRef && responsesRef.current) {
+      responsesRef.current.scrollTop = responsesRef.current.scrollHeight;
+    }
+  }, [responses, responsesRef]);
 
   return (
     <div className="App">
@@ -26,28 +35,32 @@ function App() {
         <h1>Websocket Echo Test</h1>
       </header>
       <main>
-        <form onSubmit={sendMessage}>
-          <label htmlFor="message">Send Message to Server</label><br /><br />
-          <input
-            type="text"
-            id="message"
-            placeholder="Enter message to send"
-            value={message}
-            onChange={(ev) => setMessage(ev.target.value)}
-            autoFocus
-            autoComplete="off" /> &nbsp;
-          <button>Send</button>
-        </form>
-        <hr />
         {responses.length > 0 &&
-          <ul>
-            <h2>Responses</h2>
+          <ul ref={responsesRef}>
             {
               responses.map((response, idx) => (
                 <li key={idx} className={'message ' + response.from}>{response.message}</li>
               ))
             }
           </ul>
+        }
+        {isSocketConnected
+          ? <form onSubmit={sendMessage} className='messageForm'>
+            <label htmlFor="message">Send Message to Server</label>
+            <div className="sendWidget">
+              <input
+                type="text"
+                id="message"
+                placeholder="Enter message to send"
+                value={message}
+                onChange={(ev) => setMessage(ev.target.value)}
+                autoFocus
+                autoComplete="off"
+                required />
+              <button className="sendBtn">Send</button>
+            </div>
+          </form>
+          : <div className='connecting'>Connecting to Socket.. Please wait</div>
         }
       </main>
     </div>
